90ce0ef777ca9d4c86aac888d6b1648ef975d05e
zhangjunpu <zhangjunpu050@chinasoftinc.com>
2021-08-13 15:15:18 +0800

    init repo

---
+++ app/src/main/java/com/example/resolveconflict/ContainerLayout.java
@@ -0,0 +1,82 @@
+package com.example.resolveconflict;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ContainerLayout extends ViewGroup {
+    public ContainerLayout(Context context) {
+        this(context, null);
+    }
+
+    public ContainerLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ContainerLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        measureChildren(widthMeasureSpec, heightMeasureSpec);
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        View childAt = getChildAt(0);
+        childAt.layout(0, 0, childAt.getMeasuredWidth(), childAt.getMeasuredHeight());
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                System.out.println("dispatchTouchEvent# 父亲  down");
+                break;
+            case MotionEvent.ACTION_MOVE:
+                System.out.println("dispatchTouchEvent# 父亲  move");
+                break;
+            case MotionEvent.ACTION_UP:
+                System.out.println("dispatchTouchEvent# 父亲  up");
+                break;
+        }
+        return super.dispatchTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                System.out.println("父亲拦截事件# 父亲  down");
+                break;
+            case MotionEvent.ACTION_MOVE:
+                System.out.println("父亲拦截事件# 父亲  move");
+                break;
+            case MotionEvent.ACTION_UP:
+                System.out.println("父亲拦截事件# 父亲  up");
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                System.out.println("父亲 touch 事件# 父亲  down");
+
+                break;
+            case MotionEvent.ACTION_MOVE:
+                System.out.println("父亲 touch 事件# 父亲  move");
+                break;
+            case MotionEvent.ACTION_UP:
+                System.out.println("父亲 touch 事件# 父亲  up");
+                break;
+        }
+        return super.onTouchEvent(ev);
+    }
+}
---
+++ app/src/main/java/com/example/resolveconflict/SquareView.java
@@ -0,0 +1,69 @@
+package com.example.resolveconflict;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class SquareView extends View {
+    int background = Color.parseColor("#F6F6F6");
+
+    public SquareView(Context context) {
+        this(context, null);
+    }
+
+    public SquareView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int parentWidth = MeasureSpec.getSize(widthMeasureSpec);
+        int parentHeight = MeasureSpec.getSize(heightMeasureSpec);
+        System.out.println("measure: " + parentWidth + " h: " + parentHeight);
+        setMeasuredDimension(widthMeasureSpec, parentHeight / 2);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawColor(background);
+    }
+
+    boolean flag;
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                System.out.println("dispatchTouchEvent # 孩子  down");
+                flag = true;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                System.out.println("dispatchTouchEvent # 孩子  move");
+                break;
+            case MotionEvent.ACTION_UP:
+                System.out.println("dispatchTouchEvent # 孩子  up");
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                System.out.println("孩子 onTouchEvent# 孩子  down");
+                break;
+            case MotionEvent.ACTION_MOVE:
+                System.out.println("孩子 onTouchEvent# 孩子  move");
+                break;
+            case MotionEvent.ACTION_UP:
+                System.out.println("孩子 onTouchEvent# 孩子  up");
+                break;
+        }
+        return true;
+    }
+}
